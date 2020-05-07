67ad56a6f09e5d2da96bd55407d697ec5acd2e04
Aiman <aimanaaw@gmail.com>
2020-05-07 00:14:15 -0400

    Renamed dotstar

--- regular-expressions/regex-dotStar.py
+++ regular-expressions/regex-dotStar.py
@@ -2,19 +2,20 @@ import re
 # ^ used to check for pattern in the beginning
 beginsWithHelloRegex = re.compile(r'^Hello')
 mo1 = beginsWithHelloRegex.search('Hello there!')
-print(mo1)
+# print(mo1)
 
 # Will not work
 mo2 = beginsWithHelloRegex.search('Are you there? Hello!')
-print(mo2)
+# print(mo2)
 
 # $ at the end to check for pattern at the end
 endsWithWorldRegex = re.compile(r'world!$')
 mo3 = endsWithWorldRegex.search('Hello world!')
-print(mo3)
+# print(mo3)
 
 allDigitsRegex = re.compile(r'^\d+$')
 mo4 = allDigitsRegex.search('235893458374503275')
-print(mo4)
+# print(mo4)
 mo5 = allDigitsRegex.search('23424524xdsdfsdfs')
-print(mo5)
+# print(mo5)
+
