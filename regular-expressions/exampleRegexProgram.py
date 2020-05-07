0ab37ecb0ebc8975ba7815f3625128f4865e0940
Aiman <aimanaaw@gmail.com>
2020-05-07 05:22:13 -0400

    Installed pyperclip for copy past exercise

---
+++ regular-expressions/exampleRegexProgram.py
@@ -0,0 +1,9 @@
+#! python3
+
+import re, pyperclip
+
+# TODO: Create a regex object for phone numbers
+# TODO: Create a regex object for email address
+# TODO: Get the text off the clipboard
+# TODO: Extract the email/phone from this text
+# TODO: Copy the extracted email/phone to the clipboard
