2bad994b28cab7533ff05e06b5cfbbb4906f6ae6
Aiman <aimanaaw@gmail.com>
2020-05-08 03:05:25 -0400

    Deleting files by running a for loop as a precaution

---
+++ files/files-delete.py
@@ -0,0 +1,16 @@
+import os, shutil
+
+# os.unlink('bike.py') to delete file
+
+# os.rmdir('foldername') if the folder is empty
+# shutil.rmtree('foldername') to delete a folder and all of its content
+
+# os.chdir('filePath')
+
+# for filename in os.listdir():
+# 	if filename.endswith('.txt'):
+		# os.unlink(filename)
+# 		print(filename)
+
+# Run the above loop to delete a file but comment out the delete(unlink) action
+# This lets you check the file names on your console before deletion
