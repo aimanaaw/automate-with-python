89bb34e734a1e10e115cef76b235e57c41d774fe
Aiman <aimanaaw@gmail.com>
2020-05-09 01:52:19 -0400

    Raise and exception

---
+++ debugging/debugging-raise-assert.py
@@ -0,0 +1,25 @@
+'''
+
+*********
+*		*
+*		*
+*		*
+*********
+
+'''
+
+def boxPrint(symbol, width, height):
+	if len(symbol) != 1:
+		raise Exception('Symbol needs to be length 1')
+
+	if (width < 2) or (height < 2):
+		raise Exception('Width and height needs to be greater than 2')
+	print(symbol * width)
+
+	for i in range(height - 2):
+		print(symbol + (' ' * (width - 2)) + symbol)
+
+	print(symbol * width)
+
+
+boxPrint('x', 1, 1)
