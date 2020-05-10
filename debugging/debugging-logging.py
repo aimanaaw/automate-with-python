b5a61df16f901b11022846fc58daac658f48bb91
Aiman <aimanaaw@gmail.com>
2020-05-09 22:22:55 -0400

    Logging.debug feature

--- debugging/debugging-logging.py
+++ debugging/debugging-logging.py
@@ -2,10 +2,23 @@ import logging
 
 logging.basicConfig(level = logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
+# If you want to write the logs to a file, add the filename parameter to the loggin.basicConfig function
+# logging.basicConfig(filename='myProgramming.txt', level = logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# logging.disable(logging.CRITICAL)
+
+# LOG LEVELS:
+# debug(lowest)
+# info
+# warning
+# error
+# critical(highest)
+
 logging.debug('Start a program')
 
+
 def factorial(n):
-	logging.debug('Start of factorial(%s) & (n)')
+	logging.debug('Start of factorial(%s)' % (n))
 	total = 1
 	for i in range(1, n + 1):
 		total *= i
