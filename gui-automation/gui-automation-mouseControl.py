43c9d048a7594cfe4cffd8ccdd492c166a89c6d1
Aiman <aimanaaw@gmail.com>
2020-05-13 00:10:02 -0400

    pyautogui mouse functions"

---
+++ gui-automation/gui-automation-mouseControl.py
@@ -0,0 +1,16 @@
+import pyautogui
+
+
+# print(pyautogui.size())
+
+width, height = pyautogui.size()
+
+print(pyautogui.position())
+
+# pyautogui.moveTo(1000, 1000)
+
+# print(pyautogui.position())
+
+# pyautogui.moveRel(20,0, duration=2)
+
+pyautogui.click(130, 196, duration=2)
