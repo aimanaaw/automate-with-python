011922ff2fdc3f37a08a62cb378271afcb95f17c
Aiman <aimanaaw@gmail.com>
2020-05-12 23:34:12 -0400

    How to delete emails and check for folders

--- email/email-checkInbox.py
+++ email/email-checkInbox.py
@@ -8,7 +8,7 @@ conn.log('email@gmail.com', 'password')
 conn.select_folder('INBOX', readonly=True)
 # Specify folder and readonly argument to True so you don't accidentaly delete an email
 
-# String inside a list passed into search method
+# String inside a list passed into search method. Date must be in the below format
 UIDs = conn.search(['SINCE 20-Aug-2015'])
 
 # Translate the UIDs into emails
@@ -24,3 +24,8 @@ message.get_addresses('from') #gets the recepient's email address
 message.text_part #tells you if a text part is present
 message.htmp_part #tells you if a html part is present
 message.get_payload().decode('UTF-8')
+
+conn.list_folders() #returns a list of tuples where the third item is the folder
+
+# to delete emails
+conn.select_folder('INBOX', readonly=False)
