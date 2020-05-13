d248e5be775bb2149c756dceb62d4f1d4eb8497c
Aiman <aimanaaw@gmail.com>
2020-05-12 23:04:37 -0400

    Sending mails

---
+++ email/email-sendingEmails.py
@@ -0,0 +1,16 @@
+import smtplib
+
+# server name and port number
+conn = smtplib.SMTP('smtp.gmail.com', 587)
+# print(conn)
+
+# ehlo function to start the connection
+conn.ehlo()
+# tls encryption
+conn.starttls()
+conn.login('email@gmail.com','password')
+# Subject header is 3rd argument
+conn.sendmail('sendfrom@gmail.com', 'sendto@gmail.com','Subject: So long... \n\n, \nSo long, and thanks for all the fish.\n\n')
+# {} black dictionary means the email was sent
+conn.quit()
+# Disconnected from the smtp server
