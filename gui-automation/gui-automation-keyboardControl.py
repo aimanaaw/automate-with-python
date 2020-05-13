1b5f66a8864cffdd79710f6c9732c456e146b76a
Aiman <aimanaaw@gmail.com>
2020-05-13 16:47:48 -0400

    Keyboard control) "

---
+++ gui-automation/gui-automation-keyboardControl.py
@@ -0,0 +1,5 @@
+import pyautogui
+
+pyautogui.click(1000,300, duration=2); pyautogui.typewrite('Hello World!')
+
+pyautogui.click(1000,300, duration=2); pyautogui.typewrite(['a', 'b', 'left', 'left', 'X', 'Y'], interval=1)
