38064e911be802ecc8f30fac994d452312c0b1bf
Aiman <aimanaaw@gmail.com>
2020-05-15 19:48:55 -0400

    Built in functions

---
+++ functions/functions-builtin.py
@@ -0,0 +1,13 @@
+import random, sys, os, math, pyperclip
+# The above method is preferred
+from random import *
+
+# sys.exit()
+
+print('Hello')
+# sys.exit()
+# Exits here   <====
+# print('Goodbye')
+
+pyperclip.copy('Hello World!')
+print(pyperclip.paste())
