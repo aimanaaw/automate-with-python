7632f62ff96332818436906baea45ef9e8fcd554
Aiman <aimanaaw@gmail.com>
2020-05-16 01:14:32 -0400

    Code in one functions local scope cannot use variables in any other local scope

--- functions/functions-scope.py
+++ functions/functions-scope.py
@@ -7,9 +7,20 @@
 
 # print(spam)
 # print(eggs())
-print(spam)
+# print(spam)
 
 # Global variables do not have access to a functions local scope
 # Local variables have access to the global scope
 # One functions local scope variables does not have access to another functions local scope variables
 # You can use the same name for different variables if they are in different scopes
+
+def spam():
+	eggs = 99
+	chicken()
+	print(eggs)
+
+def chicken():
+	beef = 101
+	eggs =  #chicken() eggs is separate to spam() eggs
+
+spam()
