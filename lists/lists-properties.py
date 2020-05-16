b0ae2b0fbbaa4df55ad188facfa250bc10e9ac2a
Aiman <aimanaaw@gmail.com>
2020-05-16 01:53:01 -0400

    Multiple assignment and augmented operators

---
+++ lists/lists-properties.py
@@ -0,0 +1,17 @@
+cat = ['fat', 'orange', 'loud']
+size, colour, disposition = cat
+
+print(size, colour, disposition)
+
+size, colour, disposition= cat
+print(size, colour, disposition)
+
+
+# Swapping variables
+a = 'AAA'
+b = 'BBB'
+
+a,b = b, a
+print(a ,b)
+
+# Augmented assignment variables
