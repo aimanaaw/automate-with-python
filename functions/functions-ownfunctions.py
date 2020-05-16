36286c55c09def1d6e23197aafea5f056e976df4
Aiman <aimanaaw@gmail.com>
2020-05-15 23:21:18 -0400

    Local and global scopes

---
+++ functions/functions-ownfunctions.py
@@ -0,0 +1,12 @@
+
+
+def hello():
+	print('Howdy!')
+	print('Howdy!!!')
+	print('Hello there.')
+
+
+# hello()
+# hello()
+# hello()
+print('Hello')  # return Hello. Which is a none value. It does not return Hello but prints Hello
---
+++ functions/functions-scope.py
@@ -0,0 +1,15 @@
+# spam = 42
+
+# def eggs():
+# 	spam = 5
+# 	print('spam inside eggs()', spam)
+# 	return spam
+
+# print(spam)
+# print(eggs())
+print(spam)
+
+# Global variables do not have access to a functions local scope
+# Local variables have access to the global scope
+# One functions local scope variables does not have access to another functions local scope variables
+# You can use the same name for different variables if they are in different scopes
