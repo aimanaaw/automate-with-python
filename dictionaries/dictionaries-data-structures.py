338215464f63e96e394c1f5465dc7d73d1c3a8f6
Aiman <aimanaaw@gmail.com>
2020-05-16 18:18:46 -0400

    Data structures

---
+++ dictionaries/dictionaries-data-structures.py
@@ -0,0 +1,8 @@
+cat = {'name': 'zophie', 'age': 7, 'color': 'gray'}
+allCats = []
+allCats.append({'name': 'zophie', 'age': 7, 'color': 'gray'})
+allCats.append({'name': 'pooka', 'age': 5, 'color': 'black'})
+allCats.append({'name': 'fat-tail', 'age': 5, 'color': 'gray'})
+allCats.append({'name': '???', 'age': -1, 'color': 'orange'})
+
+print(allCats)
